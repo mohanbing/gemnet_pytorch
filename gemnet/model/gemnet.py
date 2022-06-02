20ecb04e13ad7860d3f168fe125669068c42cb54
Aditya Mohan <amohan2@binghamton.edu>
2022-06-02 13:08:35 -0400

    added main func to gemnet.py for testing

--- gemnet/model/gemnet.py
+++ gemnet/model/gemnet.py
@@ -1,10 +1,10 @@
 import os
 
-try:
-    os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
-    import tensorflow as tf
-except ImportError:
-    tf = None
+# try:
+#     os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+#     import tensorflow as tf
+# except ImportError:
+#     tf = None
 
 import torch
 from torch_scatter import scatter
@@ -788,3 +788,108 @@ class GemNet(torch.nn.Module):
     
     def save_weights(self, path):
         torch.save(self.state_dict(), path)
+
+if __name__ == "__main__":
+    import yaml
+    import ast
+
+    with open('../../config.yaml', 'r') as c:
+        config = yaml.safe_load(c)
+    
+    # For strings that yaml doesn't parse (e.g. None)
+    for key, val in config.items():
+        if type(val) is str:
+            try:
+                config[key] = ast.literal_eval(val)
+            except (ValueError, SyntaxError):
+                pass
+
+    num_spherical = config["num_spherical"]
+    num_radial = config["num_radial"]
+    num_blocks = config["num_blocks"]
+    emb_size_atom = config["emb_size_atom"]
+    emb_size_edge = config["emb_size_edge"]
+    emb_size_trip = config["emb_size_trip"]
+    emb_size_quad = config["emb_size_quad"]
+    emb_size_rbf = config["emb_size_rbf"]
+    emb_size_cbf = config["emb_size_cbf"]
+    emb_size_sbf = config["emb_size_sbf"]
+    num_before_skip = config["num_before_skip"]
+    num_after_skip = config["num_after_skip"]
+    num_concat = config["num_concat"]
+    num_atom = config["num_atom"]
+    emb_size_bil_quad = config["emb_size_bil_quad"]
+    emb_size_bil_trip = config["emb_size_bil_trip"]
+    triplets_only = config["triplets_only"]
+    forces_coupled = config["forces_coupled"]
+    direct_forces = config["direct_forces"]
+    mve = config["mve"]
+    cutoff = config["cutoff"]
+    int_cutoff = config["int_cutoff"]
+    envelope_exponent = config["envelope_exponent"]
+    extensive = config["extensive"]
+    output_init = config["output_init"]
+    scale_file = config["scale_file"]
+    data_seed = config["data_seed"]
+    dataset = config["dataset"]
+    val_dataset = config["val_dataset"]
+    num_train = config["num_train"]
+    num_val = config["num_val"]
+    logdir = config["logdir"]
+    loss = config["loss"]
+    tfseed = config["tfseed"]
+    num_steps = config["num_steps"]
+    rho_force = config["rho_force"]
+    ema_decay = config["ema_decay"]
+    weight_decay = config["weight_decay"]
+    grad_clip_max = config["grad_clip_max"]
+    agc = config["agc"]
+    decay_patience = config["decay_patience"]
+    decay_factor = config["decay_factor"]
+    decay_cooldown = config["decay_cooldown"]
+    batch_size = config["batch_size"]
+    evaluation_interval = config["evaluation_interval"]
+    patience = config["patience"]
+    save_interval = config["save_interval"]
+    learning_rate = config["learning_rate"]
+    warmup_steps = config["warmup_steps"]
+    decay_steps = config["decay_steps"]
+    decay_rate = config["decay_rate"]
+    staircase = config["staircase"]
+    restart = config["restart"]
+    comment = config["comment"]
+    qm9 = config["qm9"]
+
+
+    model = GemNet(
+    num_spherical=num_spherical,
+    num_radial=num_radial,
+    num_blocks=num_blocks,
+    emb_size_atom=emb_size_atom,
+    emb_size_edge=emb_size_edge,
+    emb_size_trip=emb_size_trip,
+    emb_size_quad=emb_size_quad,
+    emb_size_rbf=emb_size_rbf,
+    emb_size_cbf=emb_size_cbf,
+    emb_size_sbf=emb_size_sbf,
+    num_before_skip=num_before_skip,
+    num_after_skip=num_after_skip,
+    num_concat=num_concat,
+    num_atom=num_atom,
+    emb_size_bil_quad=emb_size_bil_quad,
+    emb_size_bil_trip=emb_size_bil_trip,
+    num_targets=2 if mve else 1,
+    triplets_only=triplets_only,
+    direct_forces=direct_forces,
+    forces_coupled=forces_coupled,
+    cutoff=cutoff,
+    int_cutoff=int_cutoff,
+    envelope_exponent=envelope_exponent,
+    activation="swish",
+    extensive=extensive,
+    output_init=output_init,
+    scale_file=scale_file,
+    )
+
+    for para in model.parameters():
+        print(para)
